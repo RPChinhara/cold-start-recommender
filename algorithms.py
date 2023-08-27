51a987efa7047dec86c6d32060adf86e67bcd927
RPChinhara <rpchinhara@gmail.com>
2023-08-27 11:00:10 +0530

    item attribute matrix generation

--- algorithms.py
+++ algorithms.py
@@ -9,12 +9,13 @@ def item_attribute_generation_matrix(i_old, i_new, item_attributes: dict):
     A_set = set()
     IA_matrix = list()
     
+    for attributes in item_attributes.values():
+        A_set.update(attributes)
+
     for item_id in i_old:
         attributes = item_attributes.get(item_id, [])
-        A_set.update(attributes)
-        IA_matrix.append(attributes)
-
-    A_set = list(A_set)
+        attribute_vector = [1 if attr in attributes else 0 for attr in A_set]
+        IA_matrix.append(attribute_vector)
 
     for item_id in i_new:
         attributes = item_attributes.get(item_id, [])
