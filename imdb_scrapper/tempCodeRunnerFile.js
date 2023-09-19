376ec5e9d61b948fd033da5ad668e22a9b246914
Rahul kumar <rahulaki.09025@gmail.com>
2023-09-19 10:54:24 +0530

    added scrapping

---
+++ imdb_scrapper/tempCodeRunnerFile.js
@@ -0,0 +1,8 @@
+ // $(".text-muted").filter(function (){
+        //     var data = $(this)
+
+        //     attributes=data.children().text()
+
+        //     console.log(attributes)
+        //     json.attributes=attributes.trim()
+        // })
