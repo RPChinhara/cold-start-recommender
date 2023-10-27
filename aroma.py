6c64fb1a60ec2d21c7bb5d5869d2d3e1985cc63e
RPChinhara <rpchinhara@gmail.com>
2023-10-27 13:57:59 +0530

    check 3

--- aroma.py
+++ aroma.py
@@ -1,3 +1,2 @@
-def learnBPR(data, items_vectors, num_iterations=10, alpha = 0.01, alpha_b = 0.01, lambda_u = 0.1, lambda_b = 0.01):
-    for _ in range(num_iterations):
-        i, p, q = 
+def learnBPR(data, items_vectors, bias, num_iterations=10, alpha = 0.01, alpha_b = 0.01, lambda_u = 0.1, lambda_b = 0.01):
+    
