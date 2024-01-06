5356aae09c12e016c9b9c7e502701e559f7f0cff
RPChinhara <rpchinhara@gmail.com>
2024-01-06 12:38:39 +0530

    comments

--- README.md
+++ README.md
@@ -1,11 +1,13 @@
 # cold-start-recommender
 Implementation of a cold-start recommendation system using Attribute-fused Single Value Decomposition
 
+Create a conda environment using `environment.yml` file.
 
+`afsvd.py` contains algorithms code.
+
+`cold-start-recommender.ipynb` contains the implementation of the algorithms.
 
 
 
 ### References:
 X. Guo, S. -C. Yin, Y. -W. Zhang, W. Li and Q. He, "Cold Start Recommendation Based on Attribute-Fused Singular Value Decomposition," in IEEE Access, vol. 7, pp. 11349-11359, 2019, doi: 10.1109/ACCESS.2019.2891544.
-
-H. Wang et al., "A DNN-Based Cross-Domain Recommender System for Alleviating Cold-Start Problem in E-Commerce," in IEEE Open Journal of the Industrial Electronics Society, vol. 1, pp. 194-206, 2020, doi: 10.1109/OJIES.2020.3012627.
--- afsvd.py
+++ afsvd.py
@@ -12,11 +12,13 @@ def item_attribute_generation_matrix(i_old, i_new, item_attributes: dict, A_set:
     IA_matrix_old = dict()
     IA_matrix_new = dict()
 
+    # Create attribute vectors for old items
     for item_id in i_old:
         attributes = item_attributes.get(item_id, [])
         attribute_vector = tuple(1 if attr in attributes else 0 for attr in A_set)
         IA_matrix_old[item_id] = attribute_vector
 
+    # Create attribute vectors for new items
     for item_id in i_new:
         attributes = item_attributes.get(item_id, [])
         attribute_vector = tuple(1 if attr in attributes else 0 for attr in A_set)
@@ -31,25 +33,34 @@ def item_attribute_generation_matrix(i_old, i_new, item_attributes: dict, A_set:
 
 # Eq. (1)
 def ratings_based_similarity(UImatrix: pd.DataFrame, item1, item2):
+    # Get ratings for items 1 and 2
     item1_ratings = UImatrix[UImatrix['movie id'] == item1]
     item2_ratings = UImatrix[UImatrix['movie id'] == item2]
 
+    # Find common users who rated both items
     common_users = np.intersect1d(item1_ratings['user id'], item2_ratings['user id'])
+
+    # If there are not enough common users, return similarity as 0
     if len(common_users) < 2:
         return 0
     
+    # Filter ratings for common users
     item1_ratings = item1_ratings.loc[item1_ratings['user id'].isin(common_users)]
     item2_ratings = item2_ratings.loc[item2_ratings['user id'].isin(common_users)]
 
+    # Group by user and item, and calculate mean ratings
     item1_ratings = item1_ratings.groupby(['user id', 'movie id'])['rating'].mean().reset_index()
     item2_ratings = item2_ratings.groupby(['user id', 'movie id'])['rating'].mean().reset_index()
 
+    # Convert ratings to numpy arrays
     item1_ratings = item1_ratings['rating'].to_numpy()
     item2_ratings = item2_ratings['rating'].to_numpy()
 
+    # Calculate means of ratings
     item1_ratings_mean = np.mean(item1_ratings)
     item2_ratings_mean = np.mean(item2_ratings)
 
+    # Calculate Pearson correlation coefficient
     numerator = np.sum((item1_ratings - item1_ratings_mean) * (item2_ratings - item1_ratings_mean))
     denominator_x = np.sqrt(np.sum((item1_ratings - item1_ratings_mean) ** 2))
     denominator_y = np.sqrt(np.sum((item2_ratings - item2_ratings_mean) ** 2))
@@ -63,12 +74,15 @@ def ratings_based_similarity(UImatrix: pd.DataFrame, item1, item2):
 
 # Eq. (8)
 def attribute_based_similarity(IAmatrix, old_item, new_item):
+    # Get attribute vectors for old and new items
     old_item_attributes = np.array(IAmatrix['old_items'][old_item])
     new_item_attributes = np.array(IAmatrix['new_items'][new_item])
 
+    # Calculate means of attribute vectors
     old_item_attributes_mean = np.mean(old_item_attributes)
     new_item_attributes_mean = np.mean(new_item_attributes)
 
+    # Calculate denominators and numerator for similarity
     denom1 = np.sum((old_item_attributes - old_item_attributes_mean) ** 2)
     denom2 = np.sum((new_item_attributes - new_item_attributes_mean) ** 2)
 
@@ -83,24 +97,31 @@ def attribute_based_similarity(IAmatrix, old_item, new_item):
 
 # Eq. (10)
 def time_penalty(old_item_timestamp, new_item_timestamp, min_timestamp):
+    # Calculate time penalty factor
     time_penalty_factor = 1 - ((new_item_timestamp - old_item_timestamp) / (new_item_timestamp - min_timestamp))
     
     return time_penalty_factor
 
 # Eq. (11)
 def rating_prediction(IAmatrix, UImatrix: pd.DataFrame, user, item, neighbours, minimum_timestamp):
+    # Get user ratings
     user_ratings = UImatrix[UImatrix['user id'] == user]
 
+    # Calculate user mean rating
     ru_mean = user_ratings['rating'].mean()
     numerator = 0
     denominator = 0
 
     for j in neighbours:
+        # Get rating for user j and item j
         r_uj = UImatrix.loc[(UImatrix['movie id'] == j) & (UImatrix['user id'] == user), 'rating']
+
+        # Calculate similarities and time penalty
         sim_ratings = ratings_based_similarity(UImatrix, item, j)
         sim_attributes = attribute_based_similarity(IAmatrix, j, item)
         time_penalty_factor = time_penalty(UImatrix[UImatrix['movie id'] == j]['timestamp'], UImatrix[UImatrix['movie id'] == item]['timestamp'], min_timestamp=minimum_timestamp)
 
+        # Update numerator and denominator
         numerator += time_penalty_factor * sim_attributes * (r_uj - ru_mean)
         denominator += sim_ratings
 
@@ -111,18 +132,24 @@ def rating_prediction(IAmatrix, UImatrix: pd.DataFrame, user, item, neighbours, 
     return ru_mean, 0
 
 def attribute_based_knn(IAMatrix, UIMatrix: pd.DataFrame, k, users):
+    # Create a copy of the original user-item matrix
     predicted_UIMatrix = UIMatrix.copy()
     predicted_UIMatrix['user preferences'] = 0
     minimum_timestamp = predicted_UIMatrix['timestamp'].min()
 
+    # Iterate through new items
     for new_item in IAMatrix['new_items'].keys():
         similar_list = []
+
+        # Iterate through old items to calculate attribute-based similarity
         for old_item in IAMatrix['old_items'].keys():
             similar_list.append((old_item, attribute_based_similarity(IAMatrix, old_item, new_item)))
 
+        # Sort items based on similarity in descending order
         similar_list.sort(key=lambda x: x[1], reverse=True)
         neighbours = [item[0] for item in similar_list[:k]]
 
+        # Iterate through users to predict ratings
         for user_id in users:
             predicted_rating = rating_prediction(IAMatrix, predicted_UIMatrix, user_id, new_item, neighbours, minimum_timestamp)
             if isinstance(predicted_rating[0], float):
@@ -133,14 +160,18 @@ def attribute_based_knn(IAMatrix, UIMatrix: pd.DataFrame, k, users):
     return predicted_UIMatrix
 
 def matrix_factorization(train_matrix: np.ndarray, learning_rate=0.01, num_iterations=100, regularization_param=0.02, num_factors=21, eta=1):
+    # Get the dimensions of the training matrix
     num_users, num_items = train_matrix.shape
 
+    # Initialize user and item latent factor matrices with random values
     user_factors = np.random.randn(num_users, num_factors)
     item_factors = np.random.randn(num_items, num_factors)
 
+    # Calculate the mean of the entire training matrix
     u_mean = np.mean(train_matrix)
     bu = 0
 
+    # Iterate through the specified number of iterations
     while num_iterations > 0:
         for u in range(num_users):
             for i in range(num_items):
